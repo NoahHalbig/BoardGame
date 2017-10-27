952a4d3d41bc6ed108e027e9f79140e27a7e6545
NoahHalbig <33099291+NoahHalbig@users.noreply.github.com>
2017-10-27 09:50:18 -0700

    Update BoardGameProject.java

--- src/boardgameproject/BoardGameProject.java
+++ src/boardgameproject/BoardGameProject.java
@@ -4,8 +4,7 @@ public class BoardGameProject {
    
     public static void main(String[] args) {
         
-    boolean masonIsTerribleAtProgramming = true; 
-       boolean masonIsAlsoBadAtGitHub = true;
+    
        
        
     }
