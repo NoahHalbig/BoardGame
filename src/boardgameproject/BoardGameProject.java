3a0c82e89990b1ecc28a7eb6256b4f2deb2344df
Noah Halbig-145003501 <145003501@vusdnoc.vusd.k12.ca.us>
2017-10-30 11:09:16 -0700

    asdafdsd

--- src/boardgameproject/BoardGameProject.java
+++ src/boardgameproject/BoardGameProject.java
@@ -1,25 +1,191 @@
 package boardgameproject;
 
-public class BoardGameProject {
-    
+//still some problems with the shifting thing, if shifint on other players turn = a inw, other player wins = wrong
+import java.io.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class BoardGameProject extends JFrame implements Runnable {
+
+
+    boolean animateFirstTime = true;
+    Image image;
+    Graphics2D g;
+
+    static boolean gameOver;
+    static int mostRecentRow;
+    static int mostRecentCol;
+    boolean theTruth = false;
+
+
     public static void main(String[] args) {
-        int i = 0;
+        BoardGameProject frame = new BoardGameProject();
+        frame.setSize(Window.WINDOW_WIDTH, Window.WINDOW_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public BoardGameProject() {
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                
    
+                if (e.BUTTON1 == e.getButton() ) {
+                    
+                        
+                    
+                }
 
-        System.out.println("Whatever");
+                if (e.BUTTON3 == e.getButton()) {
+                    reset();
+                }
+                repaint();
+            }
+        });
+            
 
-    
-       
-        boolean thisWillWork = true;
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseDragged(MouseEvent e) {
 
-       System.out.println("Whatever");
+        repaint();
+      }
+    });
 
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseMoved(MouseEvent e) {
+
+        repaint();
+      }
+    });
+
+        addKeyListener(new KeyAdapter() {
+
+            public void keyPressed(KeyEvent e) {
+                if (e.VK_UP == e.getKeyCode()) {
+                } else if (e.VK_DOWN == e.getKeyCode()) {
+                } else if (e.VK_LEFT == e.getKeyCode()) {
+                } else if (e.VK_RIGHT == e.getKeyCode()) {
+                } else if (e.VK_ESCAPE == e.getKeyCode()) {
+                    reset();
+                }
+                repaint();
+            }
+        });
+        init();
+        start();
+    }
+    Thread relaxer;
+    public static void ChangeMostrecentRow(int changer){
+        mostRecentRow = changer;
+    }
+    public static void ChangeMostrecentColumn(int changer){
+        mostRecentCol = changer;
+    }
+    public static  void setGameOver(boolean changer){
+        gameOver = changer;
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void init() {
+        requestFocus();
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void destroy() {
+    }
+    static public boolean getGameOver(){
+        return gameOver;
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void paint(Graphics gOld) {
+        if (image == null || Window.xsize != getSize().width || Window.ysize != getSize().height) {
+            Window.xsize = getSize().width;
+            Window.ysize = getSize().height;
+            image = createImage(Window.xsize, Window.ysize);
+            g = (Graphics2D) image.getGraphics();
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+//fill background
         
-        //dfgfdgfdgfdgdfgfd
-        int mason = 99999999;
-        if(mason > 0)
-            System.out.println("Hello!");
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, Window.xsize, Window.ysize);
+
+        int x[] = {Window.getX(0), Window.getX(Window.getWidth2()), Window.getX(Window.getWidth2()), Window.getX(0), Window.getX(0)};
+        int y[] = {Window.getY(0), Window.getY(0), Window.getY(Window.getHeight2()), Window.getY(Window.getHeight2()), Window.getY(0)};
+//fill border
+        g.setColor(Color.white);
+        g.fillPolygon(x, y, 4);
+// draw border
+        g.setColor(Color.red);
+        g.drawPolyline(x, y, 5);
+
+        if (animateFirstTime) {
+            gOld.drawImage(image, 0, 0, null);
+            return;
+        }
+        
+        
+               
+        gOld.drawImage(image, 0, 0, null);
+    }
+
+////////////////////////////////////////////////////////////////////////////
+// needed for     implement runnable
+    public void run() {
+        while (true) {
+            animate();
+            repaint();
+            double seconds = .1;    //time that 1 frame takes.
+            int miliseconds = (int) (1000.0 * seconds);
+            try {
+                Thread.sleep(miliseconds);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void reset() {
+        
         
     }
-    
+/////////////////////////////////////////////////////////////////////////
+    public void animate() {
+
+        if (animateFirstTime) {
+            animateFirstTime = false;
+            if (Window.xsize != getSize().width || Window.ysize != getSize().height) {
+                Window.xsize = getSize().width;
+                Window.ysize = getSize().height;
+            }
+
+            reset();
+
+        }
+
+        
+        
+        
+    }
+
+////////////////////////////////////////////////////////////////////////////
+    public void start() {
+        if (relaxer == null) {
+            relaxer = new Thread(this);
+            relaxer.start();
+        }
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void stop() {
+        if (relaxer.isAlive()) {
+            relaxer.stop();
+        }
+        relaxer = null;
+    }
+/////////////////////////////////////////////////////////////////////////
+
 }
+
+
+
+
