68f2393255e7022152f78c36476cbc8bf146d9d3
Noah Halbig-145003501 <145003501@vusdnoc.vusd.k12.ca.us>
2017-10-30 11:03:15 -0700

    Window Making

--- src/boardgameproject/Window.java
+++ src/boardgameproject/Window.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boardgameproject;
 
-/**
- *
- * @author 155004377
- */
+
+
 public class Window {
+    static final int XBORDER = 40;
+    static final int YBORDER = 40;
+    static final int YTITLE = 25;
+    static final int WINDOW_WIDTH = 840;
+    static final int WINDOW_HEIGHT = 865;    
     
+    static int xsize = -1;
+    static int ysize = -1;
+    
+    static public int getX(int x) {
+        return (x + XBORDER);
+    }
+
+    static public int getY(int y) {
+        return (y + YBORDER + YTITLE);
+    }
+    static public int getYNormal(int y) {
+        return (-y + YBORDER + YTITLE+getHeight2());
+    }
+
+    static public int getWidth2() {
+        return (xsize - getX(0) - XBORDER);
+    }
+
+    static public int getHeight2() {
+        return (ysize - getY(0) - YBORDER);
+    }
 }
