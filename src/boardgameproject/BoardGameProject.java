c78cf4c823d81e806a9061c305793883d477f0b7
NoahHalbig <33099291+NoahHalbig@users.noreply.github.com>
2017-10-30 10:29:32 -0700

    Update BoardGameProject.java

--- src/boardgameproject/BoardGameProject.java
+++ src/boardgameproject/BoardGameProject.java
@@ -4,17 +4,16 @@ public class BoardGameProject {
     
     public static void main(String[] args) {
         int i = 0;
-<<<<<<< Updated upstream
    
 
         System.out.println("Whatever");
-=======
+
     
        
         boolean thisWillWork = true;
 
        System.out.println("Whatever");
->>>>>>> Stashed changes
+
         
         //dfgfdgfdgfdgdfgfd
         int mason = 99999999;
