0184c28217f921438348fa3acd7ca779ad590713
Noah Halbig-145003501 <145003501@vusdnoc.vusd.k12.ca.us>
2017-10-30 10:29:02 -0700

    Mason Is a lOSER

--- src/boardgameproject/BoardGameProject.java
+++ src/boardgameproject/BoardGameProject.java
@@ -4,9 +4,17 @@ public class BoardGameProject {
     
     public static void main(String[] args) {
         int i = 0;
+<<<<<<< Updated upstream
    
 
         System.out.println("Whatever");
+=======
+    
+       
+        boolean thisWillWork = true;
+
+       System.out.println("Whatever");
+>>>>>>> Stashed changes
         
         //dfgfdgfdgfdgdfgfd
         int mason = 99999999;
